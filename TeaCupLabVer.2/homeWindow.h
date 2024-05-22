44b7600f3aecdc293e5e5416ca628fc5b48329e4
渡部夏樹 <watabe-nt@tounichi-g.co.jp>
2024-05-22 11:41:41 +0900

    プロパティ設定

--- TeaCupLabVer.2/homeWindow.h
+++ TeaCupLabVer.2/homeWindow.h
@@ -62,7 +62,7 @@ namespace TeaCupLabVer2 {
 			// 
 			// button1
 			// 
-			this->button1->Location = System::Drawing::Point(384, 333);
+			this->button1->Location = System::Drawing::Point(354, 348);
 			this->button1->Name = L"button1";
 			this->button1->Size = System::Drawing::Size(156, 54);
 			this->button1->TabIndex = 3;
