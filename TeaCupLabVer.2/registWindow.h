eb190df2e42f30b8c3f37b979a9ee1e61f77e6aa
飛田武大 <tobita-tk@tounichi-g.co.jp>
2024-05-22 15:47:29 +0900

    update

--- TeaCupLabVer.2/registWindow.h
+++ TeaCupLabVer.2/registWindow.h
@@ -273,8 +273,13 @@ private: System::Void comboBox1_SelectedIndexChanged(System::Object^ sender, Sys
 	//
 	//完了ボタンを押された場合の処理
 private: System::Void buttonFinish_Click(System::Object^ sender, System::EventArgs^ e) {
-	if (this->buttonTimeStart->SelectedIndex = -1 || this->buttonMinuteStart->SelectedIndex = -1) {
-
+	if (this->buttonTimeStart->SelectedIndex == -1 || this->buttonMinuteStart->SelectedIndex == -1)
+	{
+		MessageBox::Show("終了時間が選択されていません。", "", MessageBoxButtons::OK);
+	}
+	else if (this->buttonTimeFinish->SelectedIndex == -1 || this->buttonMinuteFinish->SelectedIndex == -1)
+	{
+		MessageBox::Show("開始時間が選択されていません。", "", MessageBoxButtons::OK);
 	}
 }
 };
