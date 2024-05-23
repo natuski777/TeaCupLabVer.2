93b15738ffd382daf95c426843c5228e780bc49c
佐藤舞優 <satou-my@tounichi-g.co.jp>
2024-05-23 11:58:16 +0900

    asdddddfd

--- TeaCupLabVer.2/listWindow.h
+++ TeaCupLabVer.2/listWindow.h
@@ -1,6 +1,8 @@
 #pragma once
 #include "registWindow.h"
 #include "homeWindow.h"
+#include  <msclr/marshal_cppstd.h>
+
 
 namespace TeaCupLabVer2 {
 
@@ -10,6 +12,8 @@ namespace TeaCupLabVer2 {
 	using namespace System::Windows::Forms;
 	using namespace System::Data;
 	using namespace System::Drawing;
+	using namespace System;
+	using namespace std;
 
 	/// <summary>
 	/// listWindow の概要
@@ -43,7 +47,11 @@ namespace TeaCupLabVer2 {
 	private: System::Windows::Forms::Label^ label4;
 	private: System::Windows::Forms::Button^ button1;
 	private: System::Windows::Forms::Button^ button2;
-	private: System::Windows::Forms::TextBox^ textBox1;
+	private: System::Windows::Forms::Label^ label5;
+
+
+
+
 
 	private:
 		/// <summary>
@@ -64,7 +72,7 @@ namespace TeaCupLabVer2 {
 			this->label4 = (gcnew System::Windows::Forms::Label());
 			this->button1 = (gcnew System::Windows::Forms::Button());
 			this->button2 = (gcnew System::Windows::Forms::Button());
-			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->label5 = (gcnew System::Windows::Forms::Label());
 			this->SuspendLayout();
 			// 
 			// label1
@@ -125,20 +133,22 @@ namespace TeaCupLabVer2 {
 			this->button2->UseVisualStyleBackColor = true;
 			this->button2->Click += gcnew System::EventHandler(this, &listWindow::button2_Click);
 			// 
-			// textBox1
+			// label5
 			// 
-			this->textBox1->Location = System::Drawing::Point(26, 51);
-			this->textBox1->Name = L"textBox1";
-			this->textBox1->Size = System::Drawing::Size(100, 22);
-			this->textBox1->TabIndex = 6;
-			this->textBox1->TextChanged += gcnew System::EventHandler(this, &listWindow::textBox1_TextChanged);
+			this->label5->AutoSize = true;
+			this->label5->Location = System::Drawing::Point(33, 54);
+			this->label5->Name = L"label5";
+			this->label5->Size = System::Drawing::Size(43, 15);
+			this->label5->TabIndex = 6;
+			this->label5->Text = L"label5";
+			this->label5->Click += gcnew System::EventHandler(this, &listWindow::label5_Click);
 			// 
 			// listWindow
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(8, 15);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size(449, 359);
-			this->Controls->Add(this->textBox1);
+			this->Controls->Add(this->label5);
 			this->Controls->Add(this->button2);
 			this->Controls->Add(this->button1);
 			this->Controls->Add(this->label4);
@@ -179,10 +189,11 @@ private: System::Void button2_Click(System::Object^ sender, System::EventArgs^ e
 		Hom->ShowDialog();
 
 }
-	   
-	//カレンダーの日付を表示する処理
-private: System::Void textBox1_TextChanged(System::Object^ sender, System::EventArgs^ e) 
+	   //カレンダーの日付(date)をlabel5に表示する処理
+private: System::Void label5_Click(System::Object^ sender, System::EventArgs^ e)
 {
+	string date = gcnew string(label5->Text);
+	String^ label5 = msclr::interop::marshal_as<String^>(date);
 
 }
 };
