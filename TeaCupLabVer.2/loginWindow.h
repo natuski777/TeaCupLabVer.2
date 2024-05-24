f532293df685441079a7b8533587034be2e25566
工藤颯太 <kudou-su@tounichi-g.co.jp>
2024-05-24 14:10:00 +0900

    記号なし処理

--- TeaCupLabVer.2/loginWindow.h
+++ TeaCupLabVer.2/loginWindow.h
@@ -25,6 +25,26 @@ namespace TeaCupLabVer2
 			//
 		}
 
+		//入力されたテキストが半角英数字かどうかを判断
+		static int Alnumjudge(String^ LoginText, int len)
+		{
+			int i = 0;
+			for (i; i < len; i++)
+			{
+				if (!isalnum(LoginText[i]))
+				{
+					break;
+				}
+
+			}
+			if (i == len)
+				return 1;
+			else
+			{
+				return 0;
+			}
+		};
+
 	protected:
 		/// <summary>
 		/// 使用中のリソースをすべてクリーンアップします。
@@ -84,44 +104,44 @@ namespace TeaCupLabVer2
 			// labelUserName
 			// 
 			this->labelUserName->AutoSize = true;
-			this->labelUserName->Font = (gcnew System::Drawing::Font(L"ＭＳ Ｐゴシック", 12, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+			this->labelUserName->Font = (gcnew System::Drawing::Font(L"ＭＳ Ｐゴシック", 13.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(128)));
-			this->labelUserName->Location = System::Drawing::Point(27, 97);
+			this->labelUserName->Location = System::Drawing::Point(15, 100);
 			this->labelUserName->Name = L"labelUserName";
-			this->labelUserName->Size = System::Drawing::Size(111, 20);
+			this->labelUserName->Size = System::Drawing::Size(122, 23);
 			this->labelUserName->TabIndex = 0;
 			this->labelUserName->Text = L"ユーザー名";
 			// 
 			// labelPassword
 			// 
 			this->labelPassword->AutoSize = true;
-			this->labelPassword->Font = (gcnew System::Drawing::Font(L"ＭＳ Ｐゴシック", 12, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+			this->labelPassword->Font = (gcnew System::Drawing::Font(L"ＭＳ Ｐゴシック", 13.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(128)));
-			this->labelPassword->Location = System::Drawing::Point(28, 177);
+			this->labelPassword->Location = System::Drawing::Point(23, 171);
 			this->labelPassword->Name = L"labelPassword";
-			this->labelPassword->Size = System::Drawing::Size(104, 20);
+			this->labelPassword->Size = System::Drawing::Size(114, 23);
 			this->labelPassword->TabIndex = 1;
 			this->labelPassword->Text = L"パスワード";
 			// 
 			// textBoxUserName
 			// 
-			this->textBoxUserName->Font = (gcnew System::Drawing::Font(L"MS UI Gothic", 13.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+			this->textBoxUserName->Font = (gcnew System::Drawing::Font(L"MS UI Gothic", 18, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(128)));
 			this->textBoxUserName->ImeMode = System::Windows::Forms::ImeMode::Disable;
 			this->textBoxUserName->Location = System::Drawing::Point(143, 91);
 			this->textBoxUserName->Name = L"textBoxUserName";
-			this->textBoxUserName->Size = System::Drawing::Size(340, 30);
+			this->textBoxUserName->Size = System::Drawing::Size(340, 37);
 			this->textBoxUserName->TabIndex = 2;
 			// 
 			// textBoxPassword
 			// 
-			this->textBoxPassword->Font = (gcnew System::Drawing::Font(L"MS UI Gothic", 13.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+			this->textBoxPassword->Font = (gcnew System::Drawing::Font(L"MS UI Gothic", 18, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(128)));
 			this->textBoxPassword->ImeMode = System::Windows::Forms::ImeMode::Disable;
 			this->textBoxPassword->Location = System::Drawing::Point(143, 171);
 			this->textBoxPassword->Name = L"textBoxPassword";
 			this->textBoxPassword->PasswordChar = '*';
-			this->textBoxPassword->Size = System::Drawing::Size(340, 30);
+			this->textBoxPassword->Size = System::Drawing::Size(340, 37);
 			this->textBoxPassword->TabIndex = 3;
 			// 
 			// buttonLogin
@@ -151,11 +171,11 @@ namespace TeaCupLabVer2
 			// labelRogin
 			// 
 			this->labelRogin->AutoSize = true;
-			this->labelRogin->Font = (gcnew System::Drawing::Font(L"ＭＳ Ｐゴシック", 19.8F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+			this->labelRogin->Font = (gcnew System::Drawing::Font(L"ＭＳ Ｐゴシック", 22.2F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(128)));
-			this->labelRogin->Location = System::Drawing::Point(185, 31);
+			this->labelRogin->Location = System::Drawing::Point(184, 20);
 			this->labelRogin->Name = L"labelRogin";
-			this->labelRogin->Size = System::Drawing::Size(201, 33);
+			this->labelRogin->Size = System::Drawing::Size(227, 37);
 			this->labelRogin->TabIndex = 6;
 			this->labelRogin->Text = L"ログイン画面";
 			// 
@@ -168,9 +188,9 @@ namespace TeaCupLabVer2
 			this->labelCaution->ImageAlign = System::Drawing::ContentAlignment::MiddleRight;
 			this->labelCaution->Location = System::Drawing::Point(188, 239);
 			this->labelCaution->Name = L"labelCaution";
-			this->labelCaution->Size = System::Drawing::Size(225, 18);
+			this->labelCaution->Size = System::Drawing::Size(243, 18);
 			this->labelCaution->TabIndex = 7;
-			this->labelCaution->Text = L"※半角文字を入力してください";
+			this->labelCaution->Text = L"※半角英数字を入力してください";
 			// 
 			// loginWindow
 			// 
@@ -196,42 +216,56 @@ namespace TeaCupLabVer2
 #pragma endregion
 	private: System::Void buttonLogin_Click(System::Object^ sender, System::EventArgs^ e) 
 	{
-		 //ユーザーID,パスワードを検証し,成功した場合カレンダー画面に遷移
-		if (this->textBoxUserName->Text == "User1" && this->textBoxPassword->Text == "Pass1")
-		{
-			this->Hide();
+		//未入力時に表示されるメッセージテキストを用意
+		String^ NotEntered = "";
 
-			homeWindow^ Hom = gcnew homeWindow();
-			Hom->ShowDialog();
+		//ユーザー名またはパスワードが未入力状態の場合
+		if (this->textBoxUserName->Text == "")
+		{
+			NotEntered += "ユーザー名は必須項目です。\n";
+		}
+		if (this->textBoxPassword->Text == "")
+		{
+			NotEntered += "パスワードは必須項目です。\n";
+		}
+
+		if (NotEntered != "")
+		{
+			MessageBox::Show(NotEntered);
 		}
 		else
-		{
-			//ログインエラー発生時に表示されるメッセージテキストを用意
-			String^ loginerror = "";
+		{	//入力内容と文字数を取得
+			String^ Username = this->textBoxUserName->Text;
+			String^ Password = this->textBoxPassword->Text;
+			int lenUsername = Username->Length;
+			int lenPassword = Password->Length;
 
-			if (this->textBoxUserName->Text == "")
+			//入力テキスト内に記号が含まれていないか検証
+			//含まれているとメッセージを表示
+			if (!(Alnumjudge(Username, lenUsername) && Alnumjudge(Password, lenPassword)))
 			{
-				loginerror += "ユーザー名は必須項目です。\n";
-			}
-			if (this->textBoxPassword->Text == "")
-			{
-				loginerror += "パスワードは必須項目です。\n";
-			}
-			
+				MessageBox::Show("記号、スペースは入力できません");
 
-			if (loginerror == "")
+			}
+			//ユーザーID,パスワードを検証し,成功した場合カレンダー画面に遷移
+			else if (this->textBoxUserName->Text == "User1" && this->textBoxPassword->Text == "Pass1")
+			{
+				this->Hide();
+
+				homeWindow^ Hom = gcnew homeWindow();
+				Hom->ShowDialog();
+			}
+			//ユーザー名、パスワードの検証失敗時メッセージを表示
+			else
 			{
 				MessageBox::Show("ユーザー名またはパスワードが違います");
 			}
-			else
-			{
-				MessageBox::Show(loginerror);
-			}
-			
+
 		}
+		
 	}
 
-		//テキストボックス内の入力内容をクリア
+		//キャンセルボタンをクリック時、テキストボックス内の入力内容をクリア
 	private: System::Void buttonCancel_Click(System::Object^ sender, System::EventArgs^ e) 
 	{
 		this->textBoxUserName->Text = "";
