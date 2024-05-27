4e8a4ee77412758893659b3af99379cac68f2b98
佐藤舞優 <satou-my@tounichi-g.co.jp>
2024-05-27 14:47:17 +0900

    ｓｄｆｄｇ

--- TeaCupLabVer.2/listWindow.h
+++ TeaCupLabVer.2/listWindow.h
@@ -124,46 +124,54 @@ namespace TeaCupLabVer2
 			// label1
 			// 
 			this->label1->AutoSize = true;
-			this->label1->Font = (gcnew System::Drawing::Font(L"MS UI Gothic", 16.2F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+			this->label1->Font = (gcnew System::Drawing::Font(L"MS UI Gothic", 24, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(128)));
-			this->label1->Location = System::Drawing::Point(144, 21);
+			this->label1->Location = System::Drawing::Point(208, 9);
 			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size(180, 28);
+			this->label1->Size = System::Drawing::Size(257, 40);
 			this->label1->TabIndex = 0;
 			this->label1->Text = L"登録情報一覧";
 			// 
 			// label2
 			// 
 			this->label2->AutoSize = true;
+			this->label2->Font = (gcnew System::Drawing::Font(L"MS UI Gothic", 13.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(128)));
 			this->label2->Location = System::Drawing::Point(33, 107);
 			this->label2->Name = L"label2";
-			this->label2->Size = System::Drawing::Size(67, 15);
+			this->label2->Size = System::Drawing::Size(102, 23);
 			this->label2->TabIndex = 1;
 			this->label2->Text = L"開始時間";
 			// 
 			// label3
 			// 
 			this->label3->AutoSize = true;
+			this->label3->Font = (gcnew System::Drawing::Font(L"MS UI Gothic", 13.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(128)));
 			this->label3->Location = System::Drawing::Point(33, 167);
 			this->label3->Name = L"label3";
-			this->label3->Size = System::Drawing::Size(67, 15);
+			this->label3->Size = System::Drawing::Size(102, 23);
 			this->label3->TabIndex = 2;
 			this->label3->Text = L"終了時間";
 			// 
 			// label4
 			// 
 			this->label4->AutoSize = true;
+			this->label4->Font = (gcnew System::Drawing::Font(L"MS UI Gothic", 13.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(128)));
 			this->label4->Location = System::Drawing::Point(33, 229);
 			this->label4->Name = L"label4";
-			this->label4->Size = System::Drawing::Size(67, 15);
+			this->label4->Size = System::Drawing::Size(102, 23);
 			this->label4->TabIndex = 3;
 			this->label4->Text = L"予定内容";
 			// 
 			// buttonRegist
 			// 
-			this->buttonRegist->Location = System::Drawing::Point(286, 291);
+			this->buttonRegist->Font = (gcnew System::Drawing::Font(L"MS UI Gothic", 13.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(128)));
+			this->buttonRegist->Location = System::Drawing::Point(388, 285);
 			this->buttonRegist->Name = L"buttonRegist";
-			this->buttonRegist->Size = System::Drawing::Size(146, 39);
+			this->buttonRegist->Size = System::Drawing::Size(200, 38);
 			this->buttonRegist->TabIndex = 4;
 			this->buttonRegist->Text = L"情報登録画面へ";
 			this->buttonRegist->UseVisualStyleBackColor = true;
@@ -172,17 +180,21 @@ namespace TeaCupLabVer2
 			// labelViewDate
 			// 
 			this->labelViewDate->AutoSize = true;
-			this->labelViewDate->Location = System::Drawing::Point(33, 54);
+			this->labelViewDate->Font = (gcnew System::Drawing::Font(L"MS UI Gothic", 13.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(128)));
+			this->labelViewDate->Location = System::Drawing::Point(25, 57);
 			this->labelViewDate->Name = L"labelViewDate";
-			this->labelViewDate->Size = System::Drawing::Size(95, 15);
+			this->labelViewDate->Size = System::Drawing::Size(147, 23);
 			this->labelViewDate->TabIndex = 6;
 			this->labelViewDate->Text = L"labelViewDate";
 			// 
 			// buttonReturn
 			// 
-			this->buttonReturn->Location = System::Drawing::Point(97, 291);
+			this->buttonReturn->Font = (gcnew System::Drawing::Font(L"MS UI Gothic", 13.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(128)));
+			this->buttonReturn->Location = System::Drawing::Point(120, 285);
 			this->buttonReturn->Name = L"buttonReturn";
-			this->buttonReturn->Size = System::Drawing::Size(146, 39);
+			this->buttonReturn->Size = System::Drawing::Size(200, 38);
 			this->buttonReturn->TabIndex = 10;
 			this->buttonReturn->Text = L"カレンダー画面へ";
 			this->buttonReturn->UseVisualStyleBackColor = true;
@@ -191,88 +203,104 @@ namespace TeaCupLabVer2
 			// labelTimeStart
 			// 
 			this->labelTimeStart->AutoSize = true;
+			this->labelTimeStart->Font = (gcnew System::Drawing::Font(L"MS UI Gothic", 13.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(128)));
 			this->labelTimeStart->Location = System::Drawing::Point(168, 107);
 			this->labelTimeStart->Name = L"labelTimeStart";
-			this->labelTimeStart->Size = System::Drawing::Size(23, 15);
+			this->labelTimeStart->Size = System::Drawing::Size(0, 23);
 			this->labelTimeStart->TabIndex = 11;
-			this->labelTimeStart->Text = L"00";
 			// 
 			// label5
 			// 
 			this->label5->AutoSize = true;
-			this->label5->Location = System::Drawing::Point(221, 107);
+			this->label5->Font = (gcnew System::Drawing::Font(L"MS UI Gothic", 13.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(128)));
+			this->label5->Location = System::Drawing::Point(250, 107);
 			this->label5->Name = L"label5";
-			this->label5->Size = System::Drawing::Size(22, 15);
+			this->label5->Size = System::Drawing::Size(33, 23);
 			this->label5->TabIndex = 12;
 			this->label5->Text = L"時";
 			// 
 			// labelMinuteStart
 			// 
 			this->labelMinuteStart->AutoSize = true;
-			this->labelMinuteStart->Location = System::Drawing::Point(283, 107);
+			this->labelMinuteStart->Font = (gcnew System::Drawing::Font(L"MS UI Gothic", 13.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(128)));
+			this->labelMinuteStart->Location = System::Drawing::Point(352, 107);
 			this->labelMinuteStart->Name = L"labelMinuteStart";
-			this->labelMinuteStart->Size = System::Drawing::Size(23, 15);
+			this->labelMinuteStart->Size = System::Drawing::Size(0, 23);
 			this->labelMinuteStart->TabIndex = 13;
-			this->labelMinuteStart->Text = L"00";
 			// 
 			// label6
 			// 
 			this->label6->AutoSize = true;
-			this->label6->Location = System::Drawing::Point(358, 107);
+			this->label6->Font = (gcnew System::Drawing::Font(L"MS UI Gothic", 13.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(128)));
+			this->label6->Location = System::Drawing::Point(432, 107);
 			this->label6->Name = L"label6";
-			this->label6->Size = System::Drawing::Size(22, 15);
+			this->label6->Size = System::Drawing::Size(33, 23);
 			this->label6->TabIndex = 14;
 			this->label6->Text = L"分";
 			// 
 			// labelTimeFinish
 			// 
 			this->labelTimeFinish->AutoSize = true;
+			this->labelTimeFinish->Font = (gcnew System::Drawing::Font(L"MS UI Gothic", 13.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(128)));
 			this->labelTimeFinish->Location = System::Drawing::Point(168, 167);
 			this->labelTimeFinish->Name = L"labelTimeFinish";
-			this->labelTimeFinish->Size = System::Drawing::Size(23, 15);
+			this->labelTimeFinish->Size = System::Drawing::Size(0, 23);
 			this->labelTimeFinish->TabIndex = 15;
-			this->labelTimeFinish->Text = L"00";
 			// 
 			// label7
 			// 
 			this->label7->AutoSize = true;
-			this->label7->Location = System::Drawing::Point(221, 167);
+			this->label7->Font = (gcnew System::Drawing::Font(L"MS UI Gothic", 13.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(128)));
+			this->label7->Location = System::Drawing::Point(250, 167);
 			this->label7->Name = L"label7";
-			this->label7->Size = System::Drawing::Size(22, 15);
+			this->label7->Size = System::Drawing::Size(33, 23);
 			this->label7->TabIndex = 16;
 			this->label7->Text = L"時";
 			// 
 			// labelMinuteFinish
 			// 
 			this->labelMinuteFinish->AutoSize = true;
-			this->labelMinuteFinish->Location = System::Drawing::Point(283, 167);
+			this->labelMinuteFinish->Font = (gcnew System::Drawing::Font(L"MS UI Gothic", 13.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(128)));
+			this->labelMinuteFinish->Location = System::Drawing::Point(352, 167);
 			this->labelMinuteFinish->Name = L"labelMinuteFinish";
-			this->labelMinuteFinish->Size = System::Drawing::Size(23, 15);
+			this->labelMinuteFinish->Size = System::Drawing::Size(0, 23);
 			this->labelMinuteFinish->TabIndex = 17;
-			this->labelMinuteFinish->Text = L"00";
 			// 
 			// label8
 			// 
 			this->label8->AutoSize = true;
-			this->label8->Location = System::Drawing::Point(358, 167);
+			this->label8->Font = (gcnew System::Drawing::Font(L"MS UI Gothic", 13.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(128)));
+			this->label8->Location = System::Drawing::Point(432, 167);
 			this->label8->Name = L"label8";
-			this->label8->Size = System::Drawing::Size(22, 15);
+			this->label8->Size = System::Drawing::Size(33, 23);
 			this->label8->TabIndex = 18;
 			this->label8->Text = L"分";
 			// 
 			// labelPlan
 			// 
 			this->labelPlan->AutoSize = true;
-			this->labelPlan->Location = System::Drawing::Point(146, 229);
+			this->labelPlan->Font = (gcnew System::Drawing::Font(L"MS UI Gothic", 13.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(128)));
+			this->labelPlan->Location = System::Drawing::Point(239, 229);
 			this->labelPlan->Name = L"labelPlan";
-			this->labelPlan->Size = System::Drawing::Size(0, 15);
+			this->labelPlan->Size = System::Drawing::Size(264, 23);
 			this->labelPlan->TabIndex = 19;
+			this->labelPlan->Text = L"情報が登録されていません。";
 			// 
 			// listWindow
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(8, 15);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(502, 359);
+			this->BackColor = System::Drawing::Color::LemonChiffon;
+			this->ClientSize = System::Drawing::Size(682, 359);
 			this->Controls->Add(this->labelPlan);
 			this->Controls->Add(this->label8);
 			this->Controls->Add(this->labelMinuteFinish);
@@ -290,7 +318,8 @@ namespace TeaCupLabVer2
 			this->Controls->Add(this->label2);
 			this->Controls->Add(this->label1);
 			this->Name = L"listWindow";
-			this->Text = L"00";
+			this->StartPosition = System::Windows::Forms::FormStartPosition::CenterScreen;
+			this->Text = L"茶碗蒸し研究所";
 			this->FormClosing += gcnew System::Windows::Forms::FormClosingEventHandler(this, &listWindow::listWindow_FormClosing);
 			this->Load += gcnew System::EventHandler(this, &listWindow::listWindow_Load);
 			this->ResumeLayout(false);
@@ -343,18 +372,18 @@ namespace TeaCupLabVer2
 			getline(i_stream, str_first_conma, ',');
 
 			//先頭の文字列とdateの文字列が一致する行
+
 			if (str_first_conma == date)
 			{
+
 				//↑の中身をカンマ区切りでstr_buf配列に格納する
 				for (int i = 0; i < 5; i++)
 				{
 					getline(myFile, str_buf[i], ',');
 				}
 			}
-			else
-			{
-
-			}
+		
+			
 			//ラベルに表示するためにSystem::String型に変換
 			//開始　時間
 			string time_start =  str_buf[0];
@@ -372,12 +401,23 @@ namespace TeaCupLabVer2
 			string list_plan =  str_buf[4];
 			String^ List_Plan = msclr::interop::marshal_as<String^>(list_plan);
 
-			//ラベルに表示
-			labelTimeStart->Text = Time_Start;
-			labelMinuteStart->Text = Minute_Start;
-			labelTimeFinish->Text = Time_Finish;
-			labelMinuteFinish	->Text = Minute_Finish;
-			labelPlan->Text = List_Plan;
+			//選択した日付がテキストファイルにない場合に「情報が登録されていません。」と表示する処理
+			if (List_Plan == "")
+			{
+				labelTimeStart->Text = "0";
+				labelMinuteStart->Text = "0";
+				labelTimeFinish->Text = "0";
+				labelMinuteFinish->Text = "0";
+				labelPlan->Text = "情報が登録されていません。";
+			}
+			else
+			{
+				labelTimeStart->Text = Time_Start;
+				labelMinuteStart->Text = Minute_Start;
+				labelTimeFinish->Text = Time_Finish;
+				labelMinuteFinish->Text = Minute_Finish;
+				labelPlan->Text = List_Plan;
+			}
 
 			//MessageBox::Show(labelviewdate,"a");
 
---
+++ ソース1.cpp
@@ -0,0 +1,11 @@
+if (str_first_conma == date)
+{
+	for (int i = 0; i < 6; i++)
+	{
+		getline(myFile, str_buf[1], ',');
+	}
+}
+else
+{
+
+}
